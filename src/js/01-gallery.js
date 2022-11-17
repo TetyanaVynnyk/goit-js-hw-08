489e1c1a9a72087544b3843ce905927b30995930
TetyanaVynnyk <kharchenko1988@bigmir.net>
2022-11-17 23:11:45 +0200

    Tasks 1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 // Change code below this line
 
 console.log(galleryItems);
+
+const mainGallery = document.querySelector(".gallery");
+
+const makeGalleryItem = ({
+  preview,
+  original,
+  description,
+}) => `<div class="gallery"><a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a></div>`;
+const makeGalleryItems = galleryItems.map(makeGalleryItem).join("");
+mainGallery.insertAdjacentHTML("beforeend", makeGalleryItems);
+
+mainGallery.addEventListener("click", function (event) {
+  event.preventDefault();
+});
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
